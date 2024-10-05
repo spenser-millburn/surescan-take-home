794fdf9e01b5517abe192f3777ee86d4f2d148de
Spenser Millburn <spenser.millburn@alertinnovation.com>
2024-10-04 22:06:37 -0400

    Refactor image processing functions.

--- cpp_image_transformers/src/image_transformers/__init__.py
+++ cpp_image_transformers/src/image_transformers/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from ._core import __doc__, __version__, flipped_grayscale, flip_x, flip_y, rotate_left, rotate_right, grayscale, eigen_matrix
+from ._core import __doc__, __version__, ImageProcessor
 
-__all__ = ["__doc__", "__version__", "flipped_grayscale", "flip_x", "flip_y", "rotate_left", "rotate_right", "grayscale", "eigen_matrix"]
+__all__ = ["__doc__", "__version__", ImageProcessor]
 
--- cpp_image_transformers/src/main.cpp
+++ cpp_image_transformers/src/main.cpp
@@ -7,103 +7,76 @@
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
 
-// Function to convert OpenCV Mat to Eigen matrix
-void cv2eigen(const cv::Mat& src, Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>& dst) {
-    dst = Eigen::Map<Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>(src.data, src.rows, src.cols);
-}
-
-// Function to convert Eigen matrix back to OpenCV Mat
-void eigen2cv(const Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>& src, cv::Mat& dst) {
-    dst = cv::Mat(src.rows(), src.cols(), CV_8UC1, const_cast<unsigned char*>(src.data()));
-}
-
-// Flip image along x-axis
-pybind11::array_t<unsigned char> flip_x(const std::string &image_path, const std::string &output_path) {
-    cv::Mat image = cv::imread(image_path, cv::IMREAD_GRAYSCALE);
-    Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> eigenImage;
-    cv2eigen(image, eigenImage);
-    Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> flippedImage = eigenImage.rowwise().reverse();
-    return pybind11::array_t<unsigned char>({flippedImage.rows(), flippedImage.cols()}, flippedImage.data());
-}
-
-// Flip image along y-axis
-pybind11::array_t<unsigned char> flip_y(const std::string &image_path, const std::string &output_path) {
-    cv::Mat image = cv::imread(image_path, cv::IMREAD_GRAYSCALE);
-    Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> eigenImage;
-    cv2eigen(image, eigenImage);
-    Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> flippedImage = eigenImage.colwise().reverse();
-    return pybind11::array_t<unsigned char>({flippedImage.rows(), flippedImage.cols()}, flippedImage.data());
-}
-
-// Rotate image 90 degrees to the left
-pybind11::array_t<unsigned char> rotate_left(const std::string &image_path, const std::string &output_path) {
-    cv::Mat image = cv::imread(image_path, cv::IMREAD_GRAYSCALE);
-    Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> eigenImage;
-    cv2eigen(image, eigenImage);
-    Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> rotatedImage = eigenImage.transpose().colwise().reverse();
-    return pybind11::array_t<unsigned char>({rotatedImage.rows(), rotatedImage.cols()}, rotatedImage.data());
-}
-
-// Rotate image 90 degrees to the right
-pybind11::array_t<unsigned char> rotate_right(const std::string &image_path, const std::string &output_path) {
-    cv::Mat image = cv::imread(image_path, cv::IMREAD_GRAYSCALE);
-    Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> eigenImage;
-    cv2eigen(image, eigenImage);
-    Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> rotatedImage = eigenImage.transpose().rowwise().reverse();
-    return pybind11::array_t<unsigned char>({rotatedImage.rows(), rotatedImage.cols()}, rotatedImage.data());
-}
-
-// Convert image to grayscale (assuming input is already grayscale)
-pybind11::array_t<unsigned char> grayscale(const std::string &image_path, const std::string &output_path) {
-    cv::Mat image = cv::imread(image_path, cv::IMREAD_GRAYSCALE);
-    Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> eigenImage;
-    cv2eigen(image, eigenImage);
-    return pybind11::array_t<unsigned char>({eigenImage.rows(), eigenImage.cols()}, eigenImage.data());
-}
-
-// Return the Eigen matrix of the image
-pybind11::array_t<unsigned char> eigen_matrix(const std::string &image_path, const std::string &output_path) {
-    cv::Mat image = cv::imread(image_path, cv::IMREAD_GRAYSCALE);
-    Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> eigenImage;
-    cv2eigen(image, eigenImage);
-    return pybind11::array_t<unsigned char>({eigenImage.rows(), eigenImage.cols()}, eigenImage.data());
-}
-
-// Function to load, process, and save the image
-int flipped_grayscale(const std::string &image_path, const std::string &output_path) {
-    // Load the image
-    cv::Mat image = cv::imread(image_path, cv::IMREAD_GRAYSCALE);
-    if (image.empty()) {
-        std::cerr << "Error: Could not open or find the image." << std::endl;
-        return -1;
+class ImageProcessor {
+public:
+    ImageProcessor(const std::string &image_path) {
+        image = cv::imread(image_path, cv::IMREAD_GRAYSCALE);
+        if (image.empty()) {
+            throw std::runtime_error("Error: Could not open or find the image.");
+        }
+        cv2eigen(image, eigenImage);
     }
 
-    // Convert the image to an Eigen matrix
+    pybind11::array_t<unsigned char> flip_x() {
+        Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> flippedImage = eigenImage.rowwise().reverse();
+        return pybind11::array_t<unsigned char>({flippedImage.rows(), flippedImage.cols()}, flippedImage.data());
+    }
+
+    pybind11::array_t<unsigned char> flip_y() {
+        Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> flippedImage = eigenImage.colwise().reverse();
+        return pybind11::array_t<unsigned char>({flippedImage.rows(), flippedImage.cols()}, flippedImage.data());
+    }
+
+    pybind11::array_t<unsigned char> rotate_left() {
+        Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> rotatedImage = eigenImage.transpose().colwise().reverse();
+        return pybind11::array_t<unsigned char>({rotatedImage.rows(), rotatedImage.cols()}, rotatedImage.data());
+    }
+
+    pybind11::array_t<unsigned char> rotate_right() {
+        Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> rotatedImage = eigenImage.transpose().rowwise().reverse();
+        return pybind11::array_t<unsigned char>({rotatedImage.rows(), rotatedImage.cols()}, rotatedImage.data());
+    }
+
+    pybind11::array_t<unsigned char> grayscale() {
+        return pybind11::array_t<unsigned char>({eigenImage.rows(), eigenImage.cols()}, eigenImage.data());
+    }
+
+    pybind11::array_t<unsigned char> eigen_matrix() {
+        return pybind11::array_t<unsigned char>({eigenImage.rows(), eigenImage.cols()}, eigenImage.data());
+    }
+
+    int flipped_grayscale(const std::string &output_path) {
+        Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> flippedImage = eigenImage.colwise().reverse();
+        cv::Mat flippedMat;
+        eigen2cv(flippedImage, flippedMat);
+        cv::imwrite(output_path, flippedMat);
+        return 0; // Success
+    }
+
+private:
+    cv::Mat image;
     Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> eigenImage;
-    cv2eigen(image, eigenImage);
 
-    // Transform the image (flip it)
-    Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> flippedImage = eigenImage.colwise().reverse();
+    void cv2eigen(const cv::Mat& src, Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>& dst) {
+        dst = Eigen::Map<Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>(src.data, src.rows, src.cols);
+    }
 
-    // Convert back to OpenCV Mat
-    cv::Mat flippedMat;
-    eigen2cv(flippedImage, flippedMat);
-
-    // Save the flipped image
-    cv::imwrite(output_path, flippedMat);
-
-    return 0; // Success
-}
-
+    void eigen2cv(const Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>& src, cv::Mat& dst) {
+        dst = cv::Mat(src.rows(), src.cols(), CV_8UC1, const_cast<unsigned char*>(src.data()));
+    }
+};
 
 PYBIND11_MODULE(_core, m) {
-    m.def("flipped_grayscale", &flipped_grayscale, "A function that transforms (flips) an image and saves it to the output path.");
-    m.def("flip_x", &flip_x, "Flip image along x-axis.");
-    m.def("flip_y", &flip_y, "Flip image along y-axis.");
-    m.def("rotate_left", &rotate_left, "Rotate image 90 degrees to the left.");
-    m.def("rotate_right", &rotate_right, "Rotate image 90 degrees to the right.");
-    m.def("grayscale", &grayscale, "Convert image to grayscale.");
-    m.def("eigen_matrix", &eigen_matrix, "Return the Eigen matrix of the image.");
+    pybind11::class_<ImageProcessor>(m, "ImageProcessor")
+        .def(pybind11::init<const std::string &>())
+        .def("flipped_grayscale", &ImageProcessor::flipped_grayscale, "A function that transforms (flips) an image and saves it to the output path.")
+        .def("flip_x", &ImageProcessor::flip_x, "Flip image along x-axis.")
+        .def("flip_y", &ImageProcessor::flip_y, "Flip image along y-axis.")
+        .def("rotate_left", &ImageProcessor::rotate_left, "Rotate image 90 degrees to the left.")
+        .def("rotate_right", &ImageProcessor::rotate_right, "Rotate image 90 degrees to the right.")
+        .def("grayscale", &ImageProcessor::grayscale, "Convert image to grayscale.")
+        .def("eigen_matrix", &ImageProcessor::eigen_matrix, "Return the Eigen matrix of the image.");
+
     m.doc() = R"pbdoc(
         Pybind11 example plugin
         -----------------------
@@ -112,14 +85,9 @@ PYBIND11_MODULE(_core, m) {
 
         .. autosummary::
            :toctree: _generate
-
-           flipped_grayscale
-           flip_x
-           flip_y
-           rotate_left
-           rotate_right
-           grayscale
-           eigen_matrix
+           
+           ImageProcessor
     )pbdoc";
+
     m.attr("__version__") = "dev";
 }
