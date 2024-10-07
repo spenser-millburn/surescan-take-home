e3d370cdb68dca6c56b71ef664cad6fd20cc56e9
Spenser Millburn <spenser.millburn@alertinnovation.com>
2024-10-07 10:41:57 -0400

    [UPDATE] Refactor main into ImageBrowser class

--- qt_gui/main.cpp
+++ qt_gui/main.cpp
@@ -7,92 +7,107 @@
 #include <QWidget>
 #include <QImage>
 
-int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);
+class ImageBrowser {
+public:
+    ImageBrowser() {
+        mainWindow = new QWidget;
+        layout = new QVBoxLayout(mainWindow);
 
-    QWidget mainWindow;
-    QVBoxLayout *layout = new QVBoxLayout(&mainWindow);
+        imageLabel = new QLabel(mainWindow);
+        imageLabel->setAlignment(Qt::AlignCenter);
 
-    QLabel *imageLabel = new QLabel(&mainWindow);
-    imageLabel->setAlignment(Qt::AlignCenter); // Center the image in the label
-    QPushButton *openButton = new QPushButton("Open Image", &mainWindow);
-    QPushButton *rotateButton = new QPushButton("Rotate", &mainWindow);
-    QPushButton *flipXButton = new QPushButton("Flip X-Axis", &mainWindow);
-    QPushButton *flipYButton = new QPushButton("Flip Y-Axis", &mainWindow);
-    QPushButton *grayscaleButton = new QPushButton("Grayscale", &mainWindow);
+        openButton = new QPushButton("Open Image", mainWindow);
+        rotateButton = new QPushButton("Rotate", mainWindow);
+        flipXButton = new QPushButton("Flip X-Axis", mainWindow);
+        flipYButton = new QPushButton("Flip Y-Axis", mainWindow);
+        grayscaleButton = new QPushButton("Grayscale", mainWindow);
 
-    layout->addWidget(imageLabel);
-    layout->addWidget(openButton);
-    layout->addWidget(rotateButton);
-    layout->addWidget(flipXButton);
-    layout->addWidget(flipYButton);
-    layout->addWidget(grayscaleButton);
+        layout->addWidget(imageLabel);
+        layout->addWidget(openButton);
+        layout->addWidget(rotateButton);
+        layout->addWidget(flipXButton);
+        layout->addWidget(flipYButton);
+        layout->addWidget(grayscaleButton);
 
+        connectButtons();
+
+        mainWindow->setLayout(layout);
+        mainWindow->setWindowTitle("MyImageBrowser");
+        mainWindow->setFixedSize(800, 600);
+    }
+
+    void show() {
+        mainWindow->show();
+    }
+
+private:
+    QWidget *mainWindow;
+    QVBoxLayout *layout;
+    QLabel *imageLabel;
+    QPushButton *openButton;
+    QPushButton *rotateButton;
+    QPushButton *flipXButton;
+    QPushButton *flipYButton;
+    QPushButton *grayscaleButton;
     QImage image;
 
-    QObject::connect(openButton, &QPushButton::clicked, [&]() {
-        QString fileName = QFileDialog::getOpenFileName(&mainWindow, "Open Image File", "", "Images (*.png *.xpm *.jpg)");
-        if (!fileName.isEmpty()) {
-            image.load(fileName);
-            int fixedWidth = 400; // Fixed image width
-            int fixedHeight = 300; // Fixed image height
-            image = image.scaled(fixedWidth, fixedHeight, Qt::KeepAspectRatio, Qt::SmoothTransformation);
-            imageLabel->setPixmap(QPixmap::fromImage(image));
-        }
-    });
-
-    QObject::connect(rotateButton, &QPushButton::clicked, [&]() {
-        if (!image.isNull()) {
-            QMatrix rm;
-            rm.rotate(90);
-            image = image.transformed(rm);
-            int fixedWidth = 400;
-            int fixedHeight = 300;
-            image = image.scaled(fixedWidth, fixedHeight, Qt::KeepAspectRatio, Qt::SmoothTransformation);
-            imageLabel->setPixmap(QPixmap::fromImage(image));
-        }
-    });
-
-    QObject::connect(flipXButton, &QPushButton::clicked, [&]() {
-        if (!image.isNull()) {
-            image = image.mirrored(true, false); // Flip along the X-axis
-            int fixedWidth = 400;
-            int fixedHeight = 300;
-            image = image.scaled(fixedWidth, fixedHeight, Qt::KeepAspectRatio, Qt::SmoothTransformation);
-            imageLabel->setPixmap(QPixmap::fromImage(image));
-        }
-    });
-
-    QObject::connect(flipYButton, &QPushButton::clicked, [&]() {
-        if (!image.isNull()) {
-            image = image.mirrored(false, true); // Flip along the Y-axis
-            int fixedWidth = 400;
-            int fixedHeight = 300;
-            image = image.scaled(fixedWidth, fixedHeight, Qt::KeepAspectRatio, Qt::SmoothTransformation);
-            imageLabel->setPixmap(QPixmap::fromImage(image));
-        }
-    });
-
-    QObject::connect(grayscaleButton, &QPushButton::clicked, [&]() {
-        if (!image.isNull()) {
-            for (int y = 0; y < image.height(); ++y) {
-                for (int x = 0; x < image.width(); ++x) {
-                    QColor color = image.pixelColor(x, y);
-                    int gray = qGray(color.rgb());
-                    image.setPixelColor(x, y, QColor(gray, gray, gray));
-                }
+    void connectButtons() {
+        QObject::connect(openButton, &QPushButton::clicked, [this]() {
+            QString fileName = QFileDialog::getOpenFileName(mainWindow, "Open Image File", "", "Images (*.png *.xpm *.jpg)");
+            if (!fileName.isEmpty()) {
+                image.load(fileName);
+                updateImageDisplay();
             }
-            int fixedWidth = 400;
-            int fixedHeight = 300;
-            image = image.scaled(fixedWidth, fixedHeight, Qt::KeepAspectRatio, Qt::SmoothTransformation);
-            imageLabel->setPixmap(QPixmap::fromImage(image));
-        }
-    });
+        });
 
-    mainWindow.setLayout(layout);
-    mainWindow.setWindowTitle("MyImageBrowser");
-    mainWindow.setFixedSize(800, 600); // Set fixed window size
-    mainWindow.show();
+        QObject::connect(rotateButton, &QPushButton::clicked, [this]() {
+            if (!image.isNull()) {
+                QMatrix rm;
+                rm.rotate(90);
+                image = image.transformed(rm);
+                updateImageDisplay();
+            }
+        });
 
+        QObject::connect(flipXButton, &QPushButton::clicked, [this]() {
+            if (!image.isNull()) {
+                image = image.mirrored(true, false);
+                updateImageDisplay();
+            }
+        });
+
+        QObject::connect(flipYButton, &QPushButton::clicked, [this]() {
+            if (!image.isNull()) {
+                image = image.mirrored(false, true);
+                updateImageDisplay();
+            }
+        });
+
+        QObject::connect(grayscaleButton, &QPushButton::clicked, [this]() {
+            if (!image.isNull()) {
+                for (int y = 0; y < image.height(); ++y) {
+                    for (int x = 0; x < image.width(); ++x) {
+                        QColor color = image.pixelColor(x, y);
+                        int gray = qGray(color.rgb());
+                        image.setPixelColor(x, y, QColor(gray, gray, gray));
+                    }
+                }
+                updateImageDisplay();
+            }
+        });
+    }
+
+    void updateImageDisplay() {
+        int fixedWidth = 400;
+        int fixedHeight = 300;
+        image = image.scaled(fixedWidth, fixedHeight, Qt::KeepAspectRatio, Qt::SmoothTransformation);
+        imageLabel->setPixmap(QPixmap::fromImage(image));
+    }
+};
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    ImageBrowser browser;
+    browser.show();
     return app.exec();
 }
