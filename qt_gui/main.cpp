a8a0baf9ba7c0a0d833e79744ee6a9990aa7a7fa
Spenser Millburn <spenser.millburn@alertinnovation.com>
2024-10-07 10:08:44 -0400

    Add image manipulation features

--- qt_gui/main.cpp
+++ qt_gui/main.cpp
@@ -5,6 +5,7 @@
 #include <QFileDialog>
 #include <QPixmap>
 #include <QWidget>
+#include <QImage>
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
@@ -14,15 +15,52 @@ int main(int argc, char *argv[]) {
 
     QLabel *imageLabel = new QLabel(&mainWindow);
     QPushButton *openButton = new QPushButton("Open Image", &mainWindow);
+    QPushButton *rotateButton = new QPushButton("Rotate", &mainWindow);
+    QPushButton *flipButton = new QPushButton("Flip", &mainWindow);
+    QPushButton *grayscaleButton = new QPushButton("Grayscale", &mainWindow);
 
     layout->addWidget(imageLabel);
     layout->addWidget(openButton);
+    layout->addWidget(rotateButton);
+    layout->addWidget(flipButton);
+    layout->addWidget(grayscaleButton);
+
+    QImage image;
 
     QObject::connect(openButton, &QPushButton::clicked, [&]() {
         QString fileName = QFileDialog::getOpenFileName(&mainWindow, "Open Image File", "", "Images (*.png *.xpm *.jpg)");
         if (!fileName.isEmpty()) {
-            QPixmap image(fileName);
-            imageLabel->setPixmap(image);
+            image.load(fileName);
+            imageLabel->setPixmap(QPixmap::fromImage(image));
+        }
+    });
+
+    QObject::connect(rotateButton, &QPushButton::clicked, [&]() {
+        if (!image.isNull()) {
+            QMatrix rm;
+            rm.rotate(90);
+            image = image.transformed(rm);
+            imageLabel->setPixmap(QPixmap::fromImage(image));
+        }
+    });
+
+    QObject::connect(flipButton, &QPushButton::clicked, [&]() {
+        if (!image.isNull()) {
+            image = image.mirrored(true, false);
+            imageLabel->setPixmap(QPixmap::fromImage(image));
+        }
+    });
+
+    QObject::connect(grayscaleButton, &QPushButton::clicked, [&]() {
+        if (!image.isNull()) {
+            for (int y = 0; y < image.height(); ++y) {
+                for (int x = 0; x < image.width(); ++x) {
+                    QColor color = image.pixelColor(x, y);
+                    int gray = qGray(color.rgb());
+                    image.setPixelColor(x, y, QColor(gray, gray, gray));
+                }
+            }
+            imageLabel->setPixmap(QPixmap::fromImage(image));
         }
     });
 
