aeb10b54839cb5d2a60db2977d4273a8e7f74618
Spenser Millburn <spenser.millburn@alertinnovation.com>
2024-10-10 10:41:13 -0400

    [UPDATE] Update CMakeLists and main.cpp

--- cpp_image_transformers/src/main.cpp
+++ cpp_image_transformers/src/main.cpp
@@ -1,8 +1,13 @@
 #include "image_processor.h"
 #include <iostream>
 
-int main() {
-    ImageProcessor imgProcessor("input.jpg");
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " <input_image_filename>" << std::endl;
+        return 1;
+    }
+
+    ImageProcessor imgProcessor(argv[1]);
     imgProcessor.flipped_grayscale("./output.jpg");
 
     std::cout << "Processing complete!" << std::endl;
