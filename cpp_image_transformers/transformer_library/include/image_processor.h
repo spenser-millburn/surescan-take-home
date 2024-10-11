ffc76199bb5a376039b46810036f5fb6d1ede767
Spenser Millburn <spenser.millburn@alertinnovation.com>
2024-10-10 22:32:43 -0400

    [UPDATE] Add image processing enhancements.

--- cpp_image_transformers/transformer_library/include/image_processor.h
+++ cpp_image_transformers/transformer_library/include/image_processor.h
@@ -12,14 +12,15 @@ public:
     virtual ~Processor() = default;
 
 protected:
-    cv::Mat image;
+    cv::Mat raw_image;
     Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> eigenImage;
 
     void cv2eigen(const cv::Mat& src, Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>& dst);
     void eigen2cv(const Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>& src, cv::Mat& dst);
 };
 
-class ImageProcessor : public Processor {
+class ImageProcessor : public Processor 
+{
 public:
     ImageProcessor(const std::string &image_path);
     pybind11::array_t<unsigned char> flip_x();
@@ -27,8 +28,11 @@ public:
     pybind11::array_t<unsigned char> rotate_left();
     pybind11::array_t<unsigned char> rotate_right();
     pybind11::array_t<unsigned char> grayscale();
-    pybind11::array_t<unsigned char> eigen_matrix();
+    pybind11::array_t<unsigned char> get_image();
+
     int flipped_grayscale(const std::string &output_path);
+    void write_image(const std::string &output_path, const std::string &format);
 };
+
 
 #endif // IMAGE_PROCESSOR_H
--- cpp_image_transformers/transformer_library/src/image_processor.cpp
+++ cpp_image_transformers/transformer_library/src/image_processor.cpp
@@ -1,54 +1,79 @@
 #include "image_processor.h"
 #include <stdexcept>
 
-void Processor::cv2eigen(const cv::Mat& src, Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>& dst) {
+void Processor::cv2eigen(const cv::Mat& src, Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>& dst) 
+{
     dst = Eigen::Map<Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>(src.data, src.rows, src.cols);
 }
 
-void Processor::eigen2cv(const Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>& src, cv::Mat& dst) {
+void Processor::eigen2cv(const Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>& src, cv::Mat& dst) 
+{
     dst = cv::Mat(src.rows(), src.cols(), CV_8UC1, const_cast<unsigned char*>(src.data()));
 }
 
-ImageProcessor::ImageProcessor(const std::string &image_path) {
-    image = cv::imread(image_path, cv::IMREAD_GRAYSCALE);
-    if (image.empty()) {
+ImageProcessor::ImageProcessor(const std::string &image_path) 
+{
+    raw_image = cv::imread(image_path, cv::IMREAD_GRAYSCALE);
+    if (raw_image.empty()) 
+    {
         throw std::runtime_error("Error: Could not open or find the image.");
     }
-    cv2eigen(image, eigenImage);
+    cv2eigen(raw_image, eigenImage);
 }
 
-pybind11::array_t<unsigned char> ImageProcessor::flip_x() {
-    Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> flippedImage = eigenImage.rowwise().reverse();
-    return pybind11::array_t<unsigned char>({flippedImage.rows(), flippedImage.cols()}, flippedImage.data());
+pybind11::array_t<unsigned char> ImageProcessor::flip_x() 
+{
+    eigenImage = eigenImage.rowwise().reverse();
+    return get_image();
 }
 
-pybind11::array_t<unsigned char> ImageProcessor::flip_y() {
-    Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> flippedImage = eigenImage.colwise().reverse();
-    return pybind11::array_t<unsigned char>({flippedImage.rows(), flippedImage.cols()}, flippedImage.data());
+pybind11::array_t<unsigned char> ImageProcessor::flip_y() 
+{
+    eigenImage = eigenImage.colwise().reverse();
+    return get_image();
 }
 
-pybind11::array_t<unsigned char> ImageProcessor::rotate_left() {
-    Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> rotatedImage = eigenImage.transpose().colwise().reverse();
-    return pybind11::array_t<unsigned char>({rotatedImage.rows(), rotatedImage.cols()}, rotatedImage.data());
+pybind11::array_t<unsigned char> ImageProcessor::rotate_left() 
+{
+    eigenImage = eigenImage.transpose().colwise().reverse();
+    return get_image();
 }
 
-pybind11::array_t<unsigned char> ImageProcessor::rotate_right() {
-    Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> rotatedImage = eigenImage.transpose().rowwise().reverse();
-    return pybind11::array_t<unsigned char>({rotatedImage.rows(), rotatedImage.cols()}, rotatedImage.data());
+pybind11::array_t<unsigned char> ImageProcessor::rotate_right() 
+{
+    eigenImage = eigenImage.transpose().rowwise().reverse();
+    return get_image();
 }
 
-pybind11::array_t<unsigned char> ImageProcessor::grayscale() {
-    return pybind11::array_t<unsigned char>({eigenImage.rows(), eigenImage.cols()}, eigenImage.data());
+pybind11::array_t<unsigned char> ImageProcessor::grayscale() 
+{
+    return get_image();
 }
 
-pybind11::array_t<unsigned char> ImageProcessor::eigen_matrix() {
-    return pybind11::array_t<unsigned char>({eigenImage.rows(), eigenImage.cols()}, eigenImage.data());
-}
-
-int ImageProcessor::flipped_grayscale(const std::string &output_path) {
-    Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> flippedImage = eigenImage.colwise().reverse();
+int ImageProcessor::flipped_grayscale(const std::string &output_path) 
+{
+    eigenImage = eigenImage.colwise().reverse();
     cv::Mat flippedMat;
-    eigen2cv(flippedImage, flippedMat);
+    eigen2cv(eigenImage, flippedMat);
     cv::imwrite(output_path, flippedMat);
     return 0; // Success
+}
+
+pybind11::array_t<unsigned char> ImageProcessor::get_image()
+{
+    return pybind11::array_t<unsigned char>({eigenImage.rows(), eigenImage.cols()}, eigenImage.data());
+}
+
+void ImageProcessor::write_image(const std::string &output_path, const std::string &format) {
+    cv::Mat outputMat;
+    eigen2cv(eigenImage, outputMat);
+    std::vector<int> compression_params;
+    if (format == "jpg" || format == "jpeg") {
+        compression_params.push_back(cv::IMWRITE_JPEG_QUALITY);
+        compression_params.push_back(95); // Default quality
+    } else if (format == "png") {
+        compression_params.push_back(cv::IMWRITE_PNG_COMPRESSION);
+        compression_params.push_back(3); // Default compression level
+    }
+    cv::imwrite(output_path, outputMat, compression_params);
 }
